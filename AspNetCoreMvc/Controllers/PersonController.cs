1178ea9366c60e74cb3a4021d29f14a982d61d8b
reid <reidbras@gmail.com>
2018-05-12 15:26:27 -0500

    Adding PersonController.

---
+++ AspNetCoreMvc/Controllers/PersonController.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreMvc.Models;
+
+namespace AspNetCoreMvc.Controllers
+{
+
+
+}

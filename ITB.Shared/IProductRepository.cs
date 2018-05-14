56ac2f44071ead921b358a34b53015694fef93f5
Reid Braswell <reidbras@gmail.com>
2018-05-14 13:06:25 -0500

    Adding Shared Library ITB.Shared

---
+++ ITB.Shared/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ITB.Shared
+{
+	interface IProductRepository
+	{
+		IEnumerable<Product> GetProducts();
+		Product GetProduct(int id);
+		bool DeleteProduct(int id);
+		bool UpdateProduct(Product prod);
+		bool InsertProduct(Product prod);
+
+	}
+}
---
+++ ITB.Shared/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITB.Shared
+{
+    class Product
+    {
+    }
+}

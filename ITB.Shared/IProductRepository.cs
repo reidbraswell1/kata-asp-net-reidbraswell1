0b4ea4abd013d9e1c63ce0f726da1a4770b4647b
reid <reidbras@gmail.com>
2018-05-15 09:11:09 -0500

    Added task to IProductRepository

--- ITB.Shared/IProductRepository.cs
+++ ITB.Shared/IProductRepository.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Itb.Shared
 {
     public interface IProductRepository
     {
-		IEnumerable<Product> GetProducts();
+		Task<IEnumerable<Product>> GetProducts();
 		Product GetProduct(int id);
 		int DeleteProduct(int id);
 		int UpdateProduct(Product prod);

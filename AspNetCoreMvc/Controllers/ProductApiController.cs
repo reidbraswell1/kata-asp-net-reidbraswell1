4063081b5178d632f7732bbc1b5f0d7715816804
reid <reidbras@gmail.com>
2018-05-16 13:43:12 -0500

    Final Api Changes.

--- AspNetCoreMvc/Controllers/ProductApiController.cs
+++ AspNetCoreMvc/Controllers/ProductApiController.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ITB.Shared;
-using System.Linq;
+using System.Net.Http;
+using System.Net;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -21,41 +22,109 @@ namespace AspNetCoreMvc.Controllers
 
         // GET: api/<controller>
         [HttpGet]
-        public async Task<IEnumerable<string>> Get()
+        public async Task<IEnumerable<string>> Get([FromQuery] int id)
         {
-            var products = await _prodRepo.GetProducts();
-            string[] s = new string[products.LongCount()];
-            var i = 0;
-            foreach (var item in products)
+            if (id == 0)
             {
-                s[i++] = $"ProductId:{item.Id.ToString()},Name:{item.Name}";
+                var products = await _prodRepo.GetProducts();
+                string[] s = new string[products.LongCount()];
+                var i = 0;
+                foreach (var item in products)
+                {
+                    s[i++] = $"ProductId:{item.Id.ToString()},Name:{item.Name}";
+                }
+                return s;
             }
-            return s;
+            else
+            {
+                var product = _prodRepo.GetProduct(id);
+                return (product == null) ? new string[0] { } : new string[] { $"ProductId:{product.Id.ToString()},Name:{product.Name}" };
+            }
         }
-
-        // GET api/<controller>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
+        /* 
+                // GET api/<controller>/5
+                [HttpGet("{id}")]
+                public string Get([FromQuery] int id)
+                {
+                    var prod = _prodRepo.GetProduct(id);
+                    return $"{prod.Id.ToString()},{prod.Name}";
+                }
+        */
         // POST api/<controller>
         [HttpPost]
-        public void Post([FromBody]string value)
+        public HttpResponseMessage Post([FromBody] string name)
         {
+            var responseError = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            var responseOk = new HttpResponseMessage(HttpStatusCode.OK);
+
+            if (name == null)
+            {
+                responseError.ReasonPhrase = $"Name is Null";
+                return responseError;
+            }
+            var prod = new Product { Name = name };
+            var result = _prodRepo.AddProduct(prod);
+            if (result > 0)
+            {
+                responseOk.ReasonPhrase = $"Product:'{name}' Was Added";
+                return responseOk;
+            }
+            else
+            {
+                responseOk.ReasonPhrase = $"Product:{name} Was Not Added";
+                return responseError;
+            }
         }
 
-        // PUT api/<controller>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        // PUT api/<controller>/
+        [HttpPut]
+        public HttpResponseMessage Put([FromQuery] int id, [FromBody] string name)
         {
+            var responseError = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            var responseOk = new HttpResponseMessage(HttpStatusCode.OK);
+
+            if (id == 0 || name == null)
+            {
+                responseError.ReasonPhrase = $"ProductId:{id} is Zero or Name is Null";
+                return responseError;
+            }
+            var prod = new Product { Id = id, Name = name };
+            var result = _prodRepo.UpdateProduct(prod);
+            if (result > 0)
+            {
+                responseOk.ReasonPhrase = $"ProductId:{id} Was Updated";
+                return responseOk; ;
+            }
+            else
+            {
+                responseError.ReasonPhrase = $"ProductId:{id} Not Found in Database";
+                return responseError;
+            }
         }
 
         // DELETE api/<controller>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpDelete]
+        public HttpResponseMessage Delete([FromQuery]int id)
         {
+            var responseError = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            var responseOk = new HttpResponseMessage(HttpStatusCode.OK);
+
+            if (id == 0)
+            {
+                responseOk.ReasonPhrase = $"ProductId:{id} is Zero";
+                return responseError;
+            }
+            var result = _prodRepo.DeleteProduct(id);
+            if (result > 0)
+            {
+                responseOk.ReasonPhrase = $"ProductId:{id} Was Deleted";
+                return responseOk;
+            }
+            else
+            {
+                responseError.ReasonPhrase = $"ProductId:{id} Not Found in Database";
+                return responseError;
+            }
         }
     }
 }

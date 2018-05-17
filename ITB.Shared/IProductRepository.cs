fc24ad52c3634c6dcb9bd0fc731caa1a396093dc
reid <reidbras@gmail.com>
2018-05-17 13:19:28 -0500

    Removed Carriage Returns.

--- ITB.Shared/IProductRepository.cs
+++ ITB.Shared/IProductRepository.cs
@@ -9,7 +9,7 @@ namespace ITB.Shared
     public interface IProductRepository
     {
         Task<IEnumerable<Product>> GetProducts();
-        Product GetProduct(int id);
+        Task<IEnumerable<Product>> GetProduct(int id);
         int DeleteProduct(int id);
         int UpdateProduct(Product prod);
         int AddProduct(Product prod);
